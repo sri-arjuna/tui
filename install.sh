0e4bc2d327f7de66896ada54bf7ebfd14d4935a7
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2014-04-24 00:18:12 +0200

    Fixes in tui-newscript & implented to install script.

--- install.sh
+++ install.sh
@@ -12,19 +12,21 @@
 	DIR_CFG=/etc/$app
 	DIR_DOC=/usr/share/doc/$app
 	DIR_MAN1=/usr/share/man/man1
+	DIR_TPL=$DIR_APP/templates
 	for tDir in $DIR_APP $DIR_CFG $DIR_DOC $DIR_MAN1; do [ ! -d $tDir ] && mkdir -p $tDir;done
 #
 #	Initall to Environment
 #
 	read -p "Press enter to install $app OR press 'CTRL+c' to abort"
-	mkdir -p $DIR_APP $DIR_CFG $DIR_MAN1 #$DIR_MAN8
+	mkdir -p $DIR_APP $DIR_CFG $DIR_MAN1 $DIR_TPL #$DIR_MAN8
 	mv README.md $DIR_APP
-	mv bin/* $DIR_BIN/
-	mv conf/* $DIR_CFG/
+	mv bin/* $DIR_BIN
+	mv conf/* $DIR_CFG
 	mv docs/[A-Z]* $DIR_DOC
 	mv docs/* $DIR_APP
 	mv man/*1 $DIR_MAN1
 	mv uninstall.sh $DIR_APP
+	mv templates/ $DIR_TPL
 	
 	source tui
 	MSG="Installed $app"
