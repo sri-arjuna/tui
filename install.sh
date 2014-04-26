bef1fe6579f336103337775fdaf270a47410cbc3
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2014-04-26 16:19:25 +0200

    Make tui-browser a seperate package & small changes @ isnnstall.sh

--- install.sh
+++ install.sh
@@ -18,15 +18,15 @@
 #	Initall to Environment
 #
 	read -p "Press enter to install $app OR press 'CTRL+c' to abort"
-	mkdir -p $DIR_APP $DIR_CFG $DIR_MAN1 #$DIR_TPL #$DIR_MAN8
-	mv README.md $DIR_APP
-	mv bin/* $DIR_BIN
-	mv conf/* $DIR_CFG
-	mv docs/[A-Z]* $DIR_DOC
-	mv docs/* $DIR_APP
-	mv man/*1 $DIR_MAN1
-	mv uninstall.sh $DIR_APP
-	mv templates $DIR_TPL
+	mkdir -p $DIR_APP $DIR_CFG $DIR_MAN1 $DIR_TPL #$DIR_MAN8
+	cp README.md $DIR_APP
+	cp bin/* $DIR_BIN
+	cp conf/* $DIR_CFG
+	cp docs/[A-Z]* $DIR_DOC
+	cp docs/* $DIR_APP
+	cp man/*1 $DIR_MAN1
+	cp uninstall.sh $DIR_APP
+	cp templates/* $DIR_TPL
 	
 	source tui
 	MSG="Installed $app"
