175951aea589e648a385434c1eacfabaef9cfa84
Simon Arjuna Erat <erat.simon@gmail.com>
2014-07-23 01:32:48 +0200

    Updated install script and new application script template

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 2014.01.28 by sea
+# 2014.07.18 by sea
 #
 #	Variables
 #
@@ -13,7 +13,14 @@
 	DIR_DOC=/usr/share/doc/$app
 	DIR_MAN1=/usr/share/man/man1
 	DIR_TPL=$DIR_APP/templates
-	for tDir in $DIR_APP $DIR_CFG $DIR_DOC $DIR_MAN1; do [ ! -d $tDir ] && mkdir -p $tDir;done
+	for tDir in $DIR_APP \
+			$DIR_CFG \
+			$DIR_DOC \
+			$DIR_MAN1 \
+			$DIR_TPL
+	do	[[ ! -d $tDir ]] && \
+			mkdir -p $tDir
+	done
 #
 #	Initall to Environment
 #
@@ -21,8 +28,10 @@
 	mkdir -p $DIR_APP $DIR_CFG $DIR_MAN1 $DIR_TPL #$DIR_MAN8
 	cp README.md $DIR_APP
 	cp bin/* $DIR_BIN
-	cp conf/* $DIR_CFG
-	cp docs/* $DIR_APP
+	cp conf.etc/* $DIR_CFG
+	cp conf.home $DIR_APP/conf
+	cp docs/[A-Z]* $DIR_DOC
+	cp -R docs/* $DIR_APP
 	cp man/*1 $DIR_MAN1
 	cp uninstall.sh $DIR_APP
 	cp -R templates/* $DIR_TPL
