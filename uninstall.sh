0767603fe5d48adc5c4762d4b0520e095ce1241c
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2015-05-24 23:45:03 +0200

    Using now the ~/.tui_rc file to uninstall.

--- uninstall.sh
+++ uninstall.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
-# 2015.02.18 by sea
+# 2015.05.24 by sea
 #
 #	Variables
 #
 	app=tui
-	source /etc/tui/tui.conf ## To get the PREFIX used when installed
+	source $HOME/.tui_rc
 	[ ! -f uninstall.sh ] && cd $(dirname $0)
 	
-	# OLD
-	# DIR_APP=$PREFIX/share/$app
-	
 	# NEW
-	DIR_APP=$PWD				# Since unstall.sh is located here
-	DIR_BIN=$(dirname $(which $app))	# As the commands needs to be available, they will report their location
-	DIR_CFG=/etc/$app
-	DIR_DOC=$PREFIX/share/doc/$app
-	DIR_MAN1=$PREFIX/share/man/man1
-	COMPL=$(locate tui_compl.bash|$GREP etc)
+	DIR_APP="$TUI_DIR_SYSTEM"				# Since unstall.sh is located here
+	DIR_BIN="$TUI_PREFIX/bin"	# As the commands needs to be available, they will report their location
+	DIR_CFG="$TUI_DIR_INSTALL_ROOT/etc/$app"
+	DIR_DOC="$TUI_DIR_DOCS"
+	DIR_MAN1="$TUI_DIR_MANPAGES"
+	COMPL="$TUI_PREFIX/share/bash_completion/completions"
 #
 #	Uninstall from Environment
 #
 	tui-echo "Press enter to uninstall $app OR press 'CTRL+c' to abort"
 	tui-press
-	if tui-yesno "Remove custom configurations of all local users?"
-	then 	for usr in $(ls /home --hide=lost+found)
-		do 	tui-printf "Removing TUI configuration of $usr"
-			[ -d "/home/${usr}/.config/$app" ] && rm -fr "/home/${usr}/.config/$app"
-			tui-status $? "Removed TUI configuration of $usr"
-		done
+	if tui-bol-root
+	then	if tui-yesno "Remove custom configurations of all local users?"
+		then 	for usr in $(ls /home --hide=lost+found)
+			do 	tui-printf "Removing TUI configuration of $usr"
+				[ -d "/home/${usr}/.config/$app" ] && rm -fr "/home/${usr}/.config/$app"
+				tui-status $? "Removed TUI configuration of $usr"
+			done
+		fi
 	fi
-	rm -fr $DIR_APP
-	rm -fr $DIR_CFG
-	rm -fr $DIR_DOC
-	rm $COMPL
-	rm -f /etc/profile.d/tui.sh
+	# These are variables providing a last folder named 'tui'
+	rm -fr "$DIR_APP"
+	rm -fr "$DIR_CFG"
+	rm -fr "$DIR_DOC"
+	# Remove bash_completion
+	rm $COMPL/tui_compl.bash
+	# Remove binaries starting with 'tui'
 	rm $DIR_BIN/${app}*
-	rm $DIR_MAN1/${app}*
-	
+	# Remove manages starting with 'tui'
+	rm $DIR_MAN1/${app}* && \
+		echo -e "$TUI_DONE Removed TUI successfully.\n" || \
+		echo -e ""$TUI_FAIL Could not remove TUI.\n"
+	echo -e "\n\nThanks for having tried $app by sea.\n"
