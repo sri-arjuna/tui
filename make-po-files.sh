4e40558af278cc516f069e6d232eeb87174761ee
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2015-08-06 19:41:59 +0200

    GNU-RC9 - part 1

---
+++ make-po-files.sh
@@ -0,0 +1,10 @@
+#xgettext --extract-all --strict --debug -L Shell -o "./po/$1.po" "bin/$1"
+xgettext --debug -kMSG_TR -L Shell -o "./po/$1.po" "bin/$1"
+exit $?
+
+for d in bin #man
+do 	[ -d "$d.po" ] || mkdir "$d.po"
+	for f in ./$d/*
+	do	xgettext --extract-all --strict --debug -L Shell -o "$PWD/$d.po/${f##*/}.po" "$f"
+	done
+done
