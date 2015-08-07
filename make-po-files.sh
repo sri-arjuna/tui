aa0c23be4a44eb73fac1a2451928bda8e1b844fb
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2015-08-07 06:14:03 +0200

    RC9 - part 3

--- make-po-files.sh
+++ make-po-files.sh
@@ -1,5 +1,5 @@
 #xgettext --extract-all --strict --debug -L Shell -o "./po/$1.po" "bin/$1"
-xgettext --debug -kMSG_TR -L Shell -o "./po/$1.po" "bin/$1"
+xgettext --debug -kMSG_TR -kgettext -L Shell -o "./po/$1.po" "bin/$1"
 exit $?
 
 for d in bin #man
