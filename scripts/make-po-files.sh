eca711c1c258d48f5f1db2aa23410be57c70aa1b
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2015-08-14 05:50:25 +0200

    Some small new file arrangements

--- scripts/make-po-files.sh
+++ scripts/make-po-files.sh
@@ -1,5 +1,10 @@
 #xgettext --extract-all --strict --debug -L Shell -o "./po/$1.po" "bin/$1"
-xgettext --debug -kMSG_TR -kgettext -L Shell -o "./po/$1.po" "bin/$1"
+
+BASE=/home/sea/prjs/tui/
+cd "$BASE"
+
+xgettext --debug -kMSG_TR -kgettext -L Shell -o "./locale/en_US/$1.po" "bin/$1"
+
 exit $?
 
 for d in bin #man
