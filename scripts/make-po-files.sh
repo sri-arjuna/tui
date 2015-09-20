b3fc9d8eb00fb5b138d2119d34fa5f81c09a3480
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2015-09-20 05:06:34 +0200

    gettext update - part 1

--- scripts/make-po-files.sh
+++ scripts/make-po-files.sh
@@ -1,9 +1,38 @@
-#xgettext --extract-all --strict --debug -L Shell -o "./po/$1.po" "bin/$1"
+#!/bin/bash
+#
+#	Usage:
+#	./scripts/make-po-files.sh
+#
+#
+#	Vars
+#
+	BASE="/home/sea/prjs/tui"
+	DEST="$BASE/locale/Anticipate-po-files"
+#
+#	Functions
+#
+	getpo() { # ITEM
+	#
+	#
+		xgettext --debug -kMSG_TR -kgettext -L Shell -o "$DEST/$1.po" "bin/$1"
+	}
+#
+#	Action
+#
+	cd "$BASE"
+	mkdir -p "$DEST"
+	
+	if [ -z "$1" ]
+	then	# Ask for every file
+		for task in $(cd $BASE/bin;ls);do
+			tui-yesno "Do \"$task\"?" && getpo "$task"
+		done
+	else	# Do just this one
+		for a in "${@}";do
+			getpo "$a"
+		done
+	fi
 
-BASE=/home/sea/prjs/tui/
-cd "$BASE"
-
-xgettext --debug -kMSG_TR -kgettext -L Shell -o "./locale/en_US/$1.po" "bin/$1"
 
 exit $?
 
