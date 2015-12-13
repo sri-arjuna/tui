318532649f4d9b5680a3c751277402bf1dc74cd8
Simon Arjuna Erat (sea) <erat.simon@gmail.com>
2015-12-13 21:13:04 +0100

    0.9.3 - local testing checkpoint

--- scripts/gen-tuirc.sh
+++ scripts/gen-tuirc.sh
@@ -16,17 +16,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 # ------------------------------------------------------------------------
-#
-#	Prepare the RC file
-#
-	source ./configure.yassi || echo "Error:: Missing PATH? :: $PATH" >&2
-	source ./tui_dirs.conf
-#
-#	Write it
-#
-	cat > "./tuirc" <<EOF
+
+for tc in /usr/var/tmp /var/tmp /tmp $HOME/.cache;do [ -w $tc ] && break;done
+for tl in /usr/var/log /var/log $HOME/.config/tui/logs;do [ -w $tc ] && break;done
+
+cat << EOS
+#!/usr/bin/env bash
 # ------------------------------------------------------------------------
-# Copyright (c) 2015 Simon Arjuna Erat (sea)  <erat.simon@gmail.com>
+# Copyright (c) 1996-2015 Simon Arjuna Erat (sea)  <erat.simon@gmail.com>
 # All rights reserved.
 #
 # This program is free software: you can redistribute it and/or modify it
@@ -43,54 +40,48 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 # ------------------------------------------------------------------------
 #
-#	This file provides the default directories,
-#	required to created the user his own tuirc file.
+#	This file provides the core of the heart
+#
+	# This project
+	TUI_VERSION=$(gawk -F= '/APP_VER/ {print $2}' ./configure.yassi|head -n1)
+	# This script
+	TRC_VERSION=3.0			
 #
 #	This file is NOT meant to be changed.
 #
-	# The 'prefix', SYSCONFDIR and DATADIR variables are read from /etc/tui.conf
-	[ -z "\$TUI_DIR_CONF" ] && readonly \\
-		TUI_DIR_CONF=\$SYSCONFDIR/tui
-	[ -z "\$TUI_DIR_SYSTEM" ] && readonly \\
-		TUI_DIR_SYSTEM="\$DATADIR"
+	[ -z "\$TUI_DIR_CONF" ]		&& readonly TUI_DIR_CONF="$SYSCONFDIR/tui"
+	[ -z "\$TUI_DIR_SYSTEM" ] 	&& readonly TUI_DIR_SYSTEM="$DATADIR"
 #
 #	Paths	:	Used to run TUI
 #
-	TUI_DIR_LOG=\${prefix}/var/log
-	[ -z "\$TUI_DIR_TEMPLATES" ] && readonly \\
-		TUI_DIR_TEMPLATES="\$TUI_DIR_SYSTEM/templates"
-	[ -z "\$TUI_DIR_THEMES" ] && \\
-		TUI_DIR_THEMES="\$TUI_DIR_SYSTEM/themes"
-	[ -z "\$TUI_DIR_LIST" ] && readonly \\
-		TUI_DIR_LIST="\${TUI_DIR_SYSTEM}/lists"
-	[ -z "\$TUI_DIR_LOCALE" ] && readonly \\
-		TUI_DIR_LOCALE="\${TUI_DIR_SYSTEM}/locale"
-	TUI_DIR_TEMP="\${TUI_DIR_TEMP:-\$HOME/.cache}"
+	TUI_DIR_TEMP="$tc"
+	TUI_DIR_LOG="$tl"
+	TUI_DIR_THEMES="\$TUI_DIR_SYSTEM/themes"
+	[ -z "\$TUI_DIR_TEMPLATES" ] 	&& readonly TUI_DIR_TEMPLATES="\$TUI_DIR_SYSTEM/templates"
+	[ -z "\$TUI_DIR_FUNCTIONS" ] 	&& readonly TUI_DIR_FUNCTIONS="\$TUI_DIR_SYSTEM/functions"
+	[ -z "\$TUI_DIR_LOCALE" ]	&& readonly TUI_DIR_LOCALE="\$TUI_DIR_SYSTEM/locale"
+	[ -z "\$TUI_DIR_LIST" ]		&& readonly TUI_DIR_LIST="\$TUI_DIR_SYSTEM/lists"
 #
 #	Files
 #	Direct access to the most important configuration files for the System
 #
-	[ -z "\$TUI_FILE_CONF_APPS" ] && readonly \\
-		TUI_FILE_CONF_APPS="\$TUI_DIR_CONF/apps.conf"
-	[ -z "\$TUI_FILE_CONF_COLORS" ] && readonly \\
-		TUI_FILE_CONF_COLORS="\$TUI_DIR_CONF/colors.conf"
-	[ -z "\$TUI_FILE_CONF_COMMANDS" ] && readonly \\
-		TUI_FILE_CONF_COMMANDS="\$TUI_DIR_CONF/commands.conf"
-	[ -z "\$TUI_FILE_CONF_SETTINGS" ] && readonly \\
-		TUI_FILE_CONF_SETTINGS="\$TUI_DIR_CONF/settings.conf"
-	[ -z "\$TUI_FILE_CONF_SYSTEM" ] && readonly \\
-		TUI_FILE_CONF_SYSTEM="\$TUI_DIR_CONF/tui.conf"
-	[ -z "\$TUI_FILE_CONF_STATUS" ] && readonly \\
-		TUI_FILE_CONF_STATUS="\$TUI_DIR_CONF/status.conf"
-	[ -z "\$TUI_FILE_CONF_SHORTS" ] && readonly \\
-		TUI_FILE_CONF_SHORTS="\$TUI_DIR_CONF/shorts.conf"
-	[ -z "\$TUI_FILE_CONF_ALIAS" ] && readonly \\
-		TUI_FILE_CONF_ALIAS="\$TUI_DIR_CONF/shorts-alias.conf"
+	[ -z "\$TUI_FILE_CONF_APPS" ] 	&& readonly TUI_FILE_CONF_APPS="\$TUI_DIR_CONF/apps.conf"
+	[ -z "\$TUI_FILE_CONF_COLORS" ]	&& readonly TUI_FILE_CONF_COLORS="\$TUI_DIR_CONF/colors.conf"
+	[ -z "\$TUI_FILE_CONF_COMMANDS" ] && readonly TUI_FILE_CONF_COMMANDS="\$TUI_DIR_CONF/commands.conf"
+	[ -z "\$TUI_FILE_CONF_SETTINGS" ] && readonly TUI_FILE_CONF_SETTINGS="\$TUI_DIR_CONF/settings.conf"
+	[ -z "\$TUI_FILE_CONF_SYSTEM" ]	&& readonly TUI_FILE_CONF_SYSTEM="\$TUI_DIR_CONF/tui.conf"
+	[ -z "\$TUI_FILE_CONF_STATUS" ]	&& readonly TUI_FILE_CONF_STATUS="\$TUI_DIR_CONF/status.conf"
+	[ -z "\$TUI_FILE_CONF_SHORTS" ]	&& readonly TUI_FILE_CONF_SHORTS="\$TUI_DIR_CONF/shorts.conf"
+	[ -z "\$TUI_FILE_CONF_ALIAS" ]	&& readonly TUI_FILE_CONF_ALIAS="\$TUI_DIR_CONF/shorts-alias.conf"
 #
-#	Low level workaround
-#	As in: When called by a script during boot process
+#	Low level workarounds and 'make sures'
 #
+	HOMEBASE="$HOMEBASE"
+	HOME="\${HOME:-\$(eval echo ~\$USER)"
 	TERM="\${TERM:-GNU\057Linux}"
+	WIDTH="\${COLUMNS:-\$(tput cols)}"
+	HEIGHT="\${LINES:-\$(tput lines)}"
+	TUI_THEME="default"
 #
 #	Injection protection
 #
@@ -109,7 +100,4 @@
 		done
 		return 1
 	}
-
-EOF
-#cat "scripts/injection_protection.sh" >> ./tuirc
-sed s,"\$SYSCONFDIR/conf.tui","\$SYSCONFDIR/tui",g -i "make-uninstall"
+EOS
